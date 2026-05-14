80ae905aa46b655ef36f30e122ee7c7bc58ce885
sam8e1ito <samvelsymonian@gmail.com>
2026-05-14 11:33:11 +0200

    feat: add theme feature, basic styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import { router } from '@/app/router/router';
+import { ThemeProvider } from './app/providers/ThemeProvider';
+import './styles/main.scss';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <ThemeProvider>
+      <RouterProvider router={router} />
+    </ThemeProvider>
   </StrictMode>,
 );
